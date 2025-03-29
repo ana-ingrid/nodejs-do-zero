2b13466c891e907ef9c5a3ac9a487ed5a8a03ea0
ana-ingrid <ingridsantoscosta2003@gmail.com>
2025-03-29 00:44:51 -0300

    rotas criadas

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 import { fastify } from 'fastify'
 import { DatabaseMemory } from "./database-memory.js"
+import { title } from 'process'
 
 const server = fastify()
 const database = new DatabaseMemory()    
 
-server.post('/videos', () => {
-    return 'Hello World'
+server.post('/videos', (request, response) => {
+    database.create({
+        title: "Video 1",
+        description: "Esse é o video 01",
+        duration: 100,
+    })
+
+    console.log(database.list)
+    return response.status(201).send
+
 })
 
 server.get('/videos', () => {
